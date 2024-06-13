0f8800e4416088b7dd7d3cdb207e003279c08060
Danielle Shany <daniellesh2@mta.ac.il>
2024-06-13 19:36:06 +0300

    first commit

---
+++ http_utils.cpp
@@ -0,0 +1,74 @@
+﻿
+#include "http_utils.h"
+#include <iostream>
+#include <string.h>
+
+void handleHttpRequest(SocketState& socket)
+{
+    const char* notSupported = "HTTP/1.1 405 Method Not Allowed\r\n\r\n";
+    const char* okResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+
+    std::string request(socket.buffer);
+    std::string response;
+
+    if (request.find("OPTIONS") == 0)
+    {
+        response = "HTTP/1.1 200 OK\r\nAllow: OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE\r\n\r\n";
+    }
+    else if (request.find("GET") == 0)
+    {
+        response = okResponse;
+        if (request.find("lang=he") != std::string::npos)
+        {
+            response += "<html><body><h1>מבוא לתקשורת מחשבים - הדף של אדם ודניאל!</h1></body></html>";
+        }
+        else if (request.find("lang=en") != std::string::npos)
+        {
+            response += "<html><body><h1>Intro to Computer Communications - It's Danielle and Adam's Page!</h1></body></html>";
+        }
+        else if (request.find("lang=fr") != std::string::npos)
+        {
+            response += "<html><body><h1>Introduction aux Communications Informatiques - C'est la page de Danielle et Adam!</h1></body></html>";
+        }
+        else
+        {
+            response += "<html><body><h1>Intro to Computer Communications - It's Danielle and Adam's Page!</h1></body></html>";
+        }
+    }
+    else if (request.find("HEAD") == 0)
+    {
+        response = "HTTP/1.1 200 OK\r\n\r\n";
+    }
+    else if (request.find("POST") == 0)
+    {
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nPOST request received";
+        std::string::size_type bodyPos = request.find("\r\n\r\n");
+        if (bodyPos != std::string::npos)
+        {
+            std::string body = request.substr(bodyPos + 4);
+            std::cout << "POST body: " << body << std::endl;
+        }
+    }
+    else if (request.find("PUT") == 0)
+    {
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nPUT request received";
+    }
+    else if (request.find("DELETE") == 0)
+    {
+        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nDELETE request received";
+    }
+    else if (request.find("TRACE") == 0)
+    {
+        response = "HTTP/1.1 200 OK\r\nContent-Type: message/http\r\n\r\n";
+        response += request;
+    }
+    else
+    {
+        response = notSupported;
+    }
+
+    strcpy(socket.buffer, response.c_str());
+    socket.len = response.length();
+    socket.send = SEND;
+    socket.recv = IDLE;
+}
---
+++ server.cpp
@@ -0,0 +1,238 @@
+
+#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#include <string.h>
+#include <time.h>
+#include "http_utils.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+bool addSocket(SOCKET id, int what);
+void removeSocket(int index);
+void acceptConnection(int index);
+void receiveMessage(int index);
+void sendMessage(int index);
+
+struct SocketState sockets[MAX_SOCKETS] = { 0 };
+int socketsCount = 0;
+
+void main()
+{
+    // Initialize Winsock (Windows Sockets).
+    WSAData wsaData;
+    if (NO_ERROR != WSAStartup(MAKEWORD(2, 2), &wsaData))
+    {
+        std::cout << "HTTP Server: Error at WSAStartup()\n";
+        return;
+    }
+
+    // Create a SOCKET object called listenSocket.
+    SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (INVALID_SOCKET == listenSocket)
+    {
+        std::cout << "HTTP Server: Error at socket(): " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return;
+    }
+
+    // Create a sockaddr_in object called serverService.
+    sockaddr_in serverService;
+    serverService.sin_family = AF_INET;
+    serverService.sin_addr.s_addr = INADDR_ANY;
+    serverService.sin_port = htons(HTTP_PORT);
+
+    // Bind the socket for client's requests.
+    if (SOCKET_ERROR == bind(listenSocket, (SOCKADDR*)&serverService, sizeof(serverService)))
+    {
+        std::cout << "HTTP Server: Error at bind(): " << WSAGetLastError() << std::endl;
+        closesocket(listenSocket);
+        WSACleanup();
+        return;
+    }
+
+    // Listen on the Socket for incoming connections.
+    if (SOCKET_ERROR == listen(listenSocket, 5))
+    {
+        std::cout << "HTTP Server: Error at listen(): " << WSAGetLastError() << std::endl;
+        closesocket(listenSocket);
+        WSACleanup();
+        return;
+    }
+    addSocket(listenSocket, LISTEN);
+
+    // Accept connections and handles them one by one.
+    while (true)
+    {
+        fd_set waitRecv;
+        FD_ZERO(&waitRecv);
+        for (int i = 0; i < MAX_SOCKETS; i++)
+        {
+            if ((sockets[i].recv == LISTEN) || (sockets[i].recv == RECEIVE))
+                FD_SET(sockets[i].id, &waitRecv);
+        }
+
+        fd_set waitSend;
+        FD_ZERO(&waitSend);
+        for (int i = 0; i < MAX_SOCKETS; i++)
+        {
+            if (sockets[i].send == SEND)
+                FD_SET(sockets[i].id, &waitSend);
+        }
+
+        timeval timeout;
+        timeout.tv_sec = 2 * 60; // 2 minutes
+        timeout.tv_usec = 0;
+
+        int nfd = select(0, &waitRecv, &waitSend, NULL, &timeout);
+        if (nfd == SOCKET_ERROR)
+        {
+            std::cout << "HTTP Server: Error at select(): " << WSAGetLastError() << std::endl;
+            WSACleanup();
+            return;
+        }
+
+        for (int i = 0; i < MAX_SOCKETS && nfd > 0; i++)
+        {
+            if (FD_ISSET(sockets[i].id, &waitRecv))
+            {
+                nfd--;
+                switch (sockets[i].recv)
+                {
+                case LISTEN:
+                    acceptConnection(i);
+                    break;
+
+                case RECEIVE:
+                    receiveMessage(i);
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < MAX_SOCKETS && nfd > 0; i++)
+        {
+            if (FD_ISSET(sockets[i].id, &waitSend))
+            {
+                nfd--;
+                switch (sockets[i].send)
+                {
+                case SEND:
+                    sendMessage(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    // Closing connections and Winsock.
+    std::cout << "HTTP Server: Closing Connection.\n";
+    closesocket(listenSocket);
+    WSACleanup();
+}
+
+bool addSocket(SOCKET id, int what)
+{
+    for (int i = 0; i < MAX_SOCKETS; i++)
+    {
+        if (sockets[i].recv == EMPTY)
+        {
+            sockets[i].id = id;
+            sockets[i].recv = what;
+            sockets[i].send = IDLE;
+            sockets[i].len = 0;
+            socketsCount++;
+            return true;
+        }
+    }
+    return false;
+}
+
+void removeSocket(int index)
+{
+    sockets[index].recv = EMPTY;
+    sockets[index].send = EMPTY;
+    socketsCount--;
+}
+
+void acceptConnection(int index)
+{
+    SOCKET id = sockets[index].id;
+    struct sockaddr_in from;        // Address of sending partner
+    int fromLen = sizeof(from);
+
+    SOCKET msgSocket = accept(id, (struct sockaddr*)&from, &fromLen);
+    if (INVALID_SOCKET == msgSocket)
+    {
+        std::cout << "HTTP Server: Error at accept(): " << WSAGetLastError() << std::endl;
+        return;
+    }
+    std::cout << "HTTP Server: Client " << inet_ntoa(from.sin_addr) << ":" << ntohs(from.sin_port) << " is connected." << std::endl;
+
+    // Set the socket to be in non-blocking mode.
+    unsigned long flag = 1;
+    if (ioctlsocket(msgSocket, FIONBIO, &flag) != 0)
+    {
+        std::cout << "HTTP Server: Error at ioctlsocket(): " << WSAGetLastError() << std::endl;
+    }
+
+    if (addSocket(msgSocket, RECEIVE) == false)
+    {
+        std::cout << "\t\tToo many connections, dropped!\n";
+        closesocket(id);
+    }
+    return;
+}
+
+void receiveMessage(int index)
+{
+    SOCKET msgSocket = sockets[index].id;
+
+    int len = sockets[index].len;
+    int bytesRecv = recv(msgSocket, &sockets[index].buffer[len], sizeof(sockets[index].buffer) - len, 0);
+
+    if (SOCKET_ERROR == bytesRecv)
+    {
+        std::cout << "HTTP Server: Error at recv(): " << WSAGetLastError() << std::endl;
+        closesocket(msgSocket);
+        removeSocket(index);
+        return;
+    }
+    if (bytesRecv == 0)
+    {
+        closesocket(msgSocket);
+        removeSocket(index);
+        return;
+    }
+    else
+    {
+        sockets[index].buffer[len + bytesRecv] = '\0'; //add the null-terminating to make it a string
+        std::cout << "HTTP Server: Recieved: " << bytesRecv << " bytes of \"" << &sockets[index].buffer[len] << "\" message.\n";
+
+        sockets[index].len += bytesRecv;
+
+        if (sockets[index].len > 0)
+        {
+            handleHttpRequest(sockets[index]);
+        }
+    }
+}
+
+void sendMessage(int index)
+{
+    int bytesSent = 0;
+    SOCKET msgSocket = sockets[index].id;
+
+    bytesSent = send(msgSocket, sockets[index].buffer, sockets[index].len, 0);
+    if (SOCKET_ERROR == bytesSent)
+    {
+        std::cout << "HTTP Server: Error at send(): " << WSAGetLastError() << std::endl;
+        return;
+    }
+
+    std::cout << "HTTP Server: Sent: " << bytesSent << "\\" << sockets[index].len << " bytes of \"" << sockets[index].buffer << "\" message.\n";
+
+    sockets[index].send = IDLE;
+    sockets[index].recv = RECEIVE;
+    sockets[index].len = 0;
+}
